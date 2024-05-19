e6fb315a63cba181b58a6bf8b188b6d98ab9840f
Shivam164 <shivam-ug20@nsut.ac.in>
2024-05-19 11:43:22 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["POST", "GET"])
+def handleRequest():
+    body = request.get_json()
+    print(body)
+    return jsonify({"message": "recieved"}), 200
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run('0.0.0.0', port=4100)
