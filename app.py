cfa4ba11df14d8b2469aef12cadf4d479350c079
Shivam164 <shivam-ug20@nsut.ac.in>
2024-05-21 18:25:53 +0530

    added new webhook

--- app.py
+++ app.py
@@ -9,3 +9,14 @@ def handleRequest():
     body = request.get_json()
     print(body)
     return jsonify({"message": "recieved"}), 200
+
+@app.route("/webhook/:id", methods=["POST", "GET"])
+def handleRequest():
+    print(id)
+    body = request.get_json()
+    print(body)
+    return jsonify({"message": "recieved"}), 200
+
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run('0.0.0.0', port=4100)
